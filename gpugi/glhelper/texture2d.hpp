db3ef4347b81a3e068425ad99738a5da12697bfe
Andreas Reich (@LAPTOP) <r_andreas2@web.de>
2014-10-21 19:54:16 +0200

    Added Texture2D::SetData. (2D) Texture loading from file now possible. Not tested yet!

--- gpugi/glhelper/texture2d.hpp
+++ gpugi/glhelper/texture2d.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "texture.hpp"
+#include <memory>
+#include <string>
 
 namespace gl
 {
@@ -10,15 +12,14 @@ namespace gl
 		/// \param uiNumMipLevels   -1 for full chain, 0 and 1 have same result
 		Texture2D(std::uint32_t width, std::uint32_t height, TextureFormat format, std::int32_t numMipLevels = 1, std::uint32_t numMSAASamples = 0);
 
-		// TODO: Not reimplemented yet
 		/// \brief Loads texture from file using stb_image
 		///
 		/// Uses given to create an object if file loading is successful. Use corresponding deallocator to remove the texture.
 		/// Will return NULL on error.
-		//static Texture2D* LoadFromFile(const std::string& sFilename, bool sRGB = false, bool generateMipMaps = true);
+		static std::unique_ptr<Texture2D> LoadFromFile(const std::string& _filename, bool _generateMipMaps = true, bool _sRGB = false);
 
-		//void SetData(std::uint32_t mipLevel, const ezColor* pData);
-		//void SetData(std::uint32_t mipLevel, const unsigned char* pData);
+		/// Overwrites data of a given mip level.
+		void SetData(std::uint32_t _mipLevel, TextureSetDataFormat _dataFormat, TextureSetDataType _dataType, const void* _pData);
 
 		void GenMipMaps();
 
