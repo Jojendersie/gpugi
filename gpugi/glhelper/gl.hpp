4bcc6b247da88ab88d715e9a522ce87032353a5e
Andreas Reich <r_andreas2@web.de>
2014-11-03 19:55:56 +0100

    GL call wrapper uses now variadic templates.

--- gpugi/glhelper/gl.hpp
+++ gpugi/glhelper/gl.hpp
@@ -2,6 +2,7 @@
 
 #include "../utilities/utils.hpp"
 #include <GL/glew.h>
+#include <type_traits>
 
 namespace gl
 {
@@ -45,95 +46,48 @@ namespace gl
 	/// \returns false if fkt is nullptr.
 	bool CheckGLFunctionExistsAndReport(const char* openGLFunctionName, const void* fkt);
 
-#if _DEBUG
-
-	// Internal macro that defines what happens on an OpenGL call.
-	/// Macro for normal gl call.
-#define __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, ...) \
-			{ \
-		if (!CheckGLFunctionExistsAndReport(openGLFunctionName, openGLFunction)) \
-			return FAILURE; \
-		openGLFunction(__VA_ARGS__); \
-		return CheckGLError(openGLFunctionName); \
-			}
-
-	/// Macro for gl call with return value.
-#define __GL_CALL_HANDLING_RET(openGLFunctionName, openGLFunction, ...) \
-			{ \
-		if (!CheckGLFunctionExistsAndReport	(openGLFunctionName, openGLFunction)) \
-			return 0; \
-		auto out = openGLFunction(__VA_ARGS__); \
-		CheckGLError(openGLFunctionName); \
-		return out; \
-			}
-
-#else
-
-	// Internal macro that defines what happens on an OpenGL call.
-#define __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, ...) \
-			{ \
-		openGLFunction(__VA_ARGS__); \
-		return ::SUCCEEDED; \
-			}
-
-#define __GL_CALL_HANDLING_RET(openGLFunctionName, openGLFunction, ...) \
-			{ \
-		return openGLFunction(__VA_ARGS__); \
-			}
-
-#endif
 
 	namespace Details
 	{
-		// GL call helper. The advantage of template function instead of a mere macro is that it can also be used in conditions!
-		// The whole thing would be of course much easier with variadic templates. But MSVC <=120 lacking support forces us to do this.
-		template<typename GlFunction, typename Arg0>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0);
-		template<typename GlFunction, typename Arg0, typename Arg1>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3, arg4);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3, arg4, arg5);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3, arg4, arg5, arg6);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8, typename Arg9>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3, typename Arg4, typename Arg5, typename Arg6, typename Arg7, typename Arg8, typename Arg9, typename Arg10>
-		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8, Arg9 arg9, Arg10 arg10) __GL_CALL_HANDLING(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+		template<typename GlFunction, typename... Args>
+		Result CheckedGLCall(const char* openGLFunctionName, GlFunction openGLFunction, Args&&... args)
+		{
+			if (!CheckGLFunctionExistsAndReport(openGLFunctionName, openGLFunction))
+				return FAILURE;
+			openGLFunction(args...);
+			return CheckGLError(openGLFunctionName);
+		}
+		
 
-
-		template<typename GlFunction>
-		auto CheckedGLCall_Ret(const char* openGLFunctionName, GlFunction openGLFunction) -> decltype(openGLFunction()) __GL_CALL_HANDLING_RET(openGLFunctionName, openGLFunction);
-		template<typename GlFunction, typename Arg0>
-		auto CheckedGLCall_Ret(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0) -> decltype(openGLFunction(arg0)) __GL_CALL_HANDLING_RET(openGLFunctionName, openGLFunction, arg0);
-		template<typename GlFunction, typename Arg0, typename Arg1>
-		auto CheckedGLCall_Ret(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1) -> decltype(openGLFunction(arg0, arg1)) __GL_CALL_HANDLING_RET(openGLFunctionName, openGLFunction, arg0, arg1);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2>
-		auto CheckedGLCall_Ret(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2) -> decltype(openGLFunction(arg0, arg1, arg2)) __GL_CALL_HANDLING_RET(openGLFunctionName, openGLFunction, arg0, arg1, arg2);
-		template<typename GlFunction, typename Arg0, typename Arg1, typename Arg2, typename Arg3>
-		auto CheckedGLCall_Ret(const char* openGLFunctionName, GlFunction openGLFunction, Arg0 arg0, Arg1 arg1, Arg2 arg2, Arg3 arg3) -> decltype(openGLFunction(arg0, arg1, arg2, arg3)) __GL_CALL_HANDLING_RET(openGLFunctionName, openGLFunction, arg0, arg1, arg2, arg3);
+		template<typename ReturnType, typename GlFunction, typename... Args>
+		ReturnType CheckedGLCall_Ret(const char* openGLFunctionName, GlFunction openGLFunction, Args&&... args)
+		{
+			if (!CheckGLFunctionExistsAndReport(openGLFunctionName, openGLFunction))
+				return 0;
+			ReturnType out = openGLFunction(args...);
+			CheckGLError(openGLFunctionName);
+			return out;
+		}
 	}
 
-	// Remove internal call handling helper macro.
-#undef __GL_CALL_HANDLING
-#undef __GL_CALL_HANDLING_RET
+#ifdef _DEBUG
 
 	/// Recommend way to call any OpenGL function. Will perform optional nullptr and glGetError checks.
 #define GL_CALL(OpenGLFunction, ...) \
-	::gl::Details::CheckedGLCall(#OpenGLFunction, OpenGLFunction, __VA_ARGS__)
+	do { ::gl::Details::CheckedGLCall(#OpenGLFunction, OpenGLFunction, __VA_ARGS__); } while(false)
 
 	/// There are a few functions that have a return value (glGet, glIsX, glCreateShader, glCreateProgram). Use this macro for those.
 #define GL_RET_CALL(OpenGLFunction, ...) \
-	::gl::Details::CheckedGLCall_Ret(#OpenGLFunction, OpenGLFunction, __VA_ARGS__)
+	::gl::Details::CheckedGLCall_Ret<decltype(OpenGLFunction(__VA_ARGS__))>(#OpenGLFunction, OpenGLFunction, __VA_ARGS__)
+
+#else
+
+#define GL_CALL(OpenGLFunction, ...) OpenGLFunction(__VA_ARGS__)
+
+#define GL_RET_CALL(OpenGLFunction, ...) OpenGLFunction(__VA_ARGS__)
+
+#endif
+
 
 	/// There is another set of functions which a few functions that have a return value (glGet, glIsX, glCreateShader, glCreateProgram). Use this macro for those.
 //#define GL_RET_CALL(OpenGLFunction, ...) \
