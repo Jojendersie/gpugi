dce064f0ebcd50bbad6e27b6111def7cbf9b8be8
Johannes Jendersie <jojendersie@web.de>
2014-11-05 16:53:05 +0100

    if-if optimization for TraceRay

--- gpugi/shader/pathtracer_traceray.glsl
+++ gpugi/shader/pathtracer_traceray.glsl
@@ -8,67 +8,82 @@
 #endif
 {
 	int currentNodeIndex = 0;
+	int currentLeafIndex;
 	vec3 invRayDir = 1.0 / ray.Direction;
+	bool nextIsLeafNode = false;
 
 	do {
-		// Load entire node.
-		vec4 currentNode0 = texelFetch(HierachyBuffer, currentNodeIndex * 2);
-		vec4 currentNode1 = texelFetch(HierachyBuffer, currentNodeIndex * 2 + 1);
-
-		#ifdef TRACERAY_DEBUG_VARS
-			++numBoxesVisited;
-		#endif
-
-		float newHit;
-		if(IntersectBox(ray, invRayDir, currentNode0.xyz, currentNode1.xyz, newHit) && newHit <= rayLength)
+		if(!nextIsLeafNode)
 		{
-			uint childCode = floatBitsToUint(currentNode0.w);
-			currentNodeIndex = int(childCode & uint(0x7FFFFFFF));  // Most significant bit tells us is this is a leaf.
-		
-			// If it is a leaf ...
-			if(currentNodeIndex != childCode)
+			// Load entire node.
+			vec4 currentNode0 = texelFetch(HierachyBuffer, currentNodeIndex * 2);
+			vec4 currentNode1 = texelFetch(HierachyBuffer, currentNodeIndex * 2 + 1);
+
+			#ifdef TRACERAY_DEBUG_VARS
+				++numBoxesVisited;
+			#endif
+
+			float newHit;
+			if(IntersectBox(ray, invRayDir, currentNode0.xyz, currentNode1.xyz, newHit) && newHit <= rayLength)
 			{
-				for(int i=0; i<TRIANGLES_PER_LEAF; ++i)
+				uint childCode = floatBitsToUint(currentNode0.w);
+				// Most significant bit tells us is this is a leaf.
+				currentNodeIndex = int(childCode & uint(0x7FFFFFFF));
+				nextIsLeafNode = currentNodeIndex != childCode;
+				// If we go into leave mode...
+				if(nextIsLeafNode)
 				{
-					// Load triangle.
-					Triangle triangle = texelFetch(TriangleBuffer, int(TRIANGLES_PER_LEAF * currentNodeIndex + i));
-					if(triangle.x == triangle.y) // Last test if optimization or perf decline: 02.11.14
-						continue;
-						
-					#ifdef TRACERAY_DEBUG_VARS
-						++numTrianglesVisited;
-					#endif
-
-					// Load vertex positions
-					vec3 positions[3];
-					positions[0] = texelFetch(VertexBuffer, triangle.x * 2).xyz;//Vertices[triangle.x].Position;
-					positions[1] = texelFetch(VertexBuffer, triangle.y * 2).xyz;//Vertices[triangle.y].Position;
-					positions[2] = texelFetch(VertexBuffer, triangle.z * 2).xyz;//Vertices[triangle.z].Position;
-
-					// Check hit.
-					vec3 triangleNormal;
-					float newHit; vec3 newBarycentricCoord;
-					if(IntersectTriangle(ray, positions[0], positions[1], positions[2], newHit, newBarycentricCoord, triangleNormal) && newHit < rayLength)
-					{
-						#ifdef ANY_HIT
-							return true;
-						#else
-							rayLength = newHit;
-							outTriangle = triangle;
-							outBarycentricCoord = newBarycentricCoord;
-							// Cannot return yet, there might be a triangle that is hit before this one!
-						#endif
-					}
+					currentLeafIndex = int(TRIANGLES_PER_LEAF * currentNodeIndex);
+					currentNodeIndex = floatBitsToInt(currentNode1.w);
 				}
+			}
+			// No hit, go to escape pointer and repeat.
+			else
+			{
 				currentNodeIndex = floatBitsToInt(currentNode1.w);
 			}
-		}
-		// No hit, go to escape pointer and repeat.
-		else
+		}	
+		
+		// If it is a leaf ...
+		// The nextIsLeafNode can be changed since 'if(!nextIsLeafNode)', so do no 'else' here
+		if(nextIsLeafNode)
 		{
-			currentNodeIndex = floatBitsToInt(currentNode1.w);
+			// Load triangle.
+			Triangle triangle = texelFetch(TriangleBuffer, currentLeafIndex);
+			if(triangle.x == triangle.y)
+			{
+				nextIsLeafNode = false;
+			} else {		
+				#ifdef TRACERAY_DEBUG_VARS
+					++numTrianglesVisited;
+				#endif
+
+				// Load vertex positions
+				vec3 positions[3];
+				positions[0] = texelFetch(VertexBuffer, triangle.x * 2).xyz;//Vertices[triangle.x].Position;
+				positions[1] = texelFetch(VertexBuffer, triangle.y * 2).xyz;//Vertices[triangle.y].Position;
+				positions[2] = texelFetch(VertexBuffer, triangle.z * 2).xyz;//Vertices[triangle.z].Position;
+
+				// Check hit.
+				vec3 triangleNormal;
+				float newHit; vec3 newBarycentricCoord;
+				if(IntersectTriangle(ray, positions[0], positions[1], positions[2], newHit, newBarycentricCoord, triangleNormal) && newHit < rayLength)
+				{
+					#ifdef ANY_HIT
+						return true;
+					#else
+						rayLength = newHit;
+						outTriangle = triangle;
+						outBarycentricCoord = newBarycentricCoord;
+						// Cannot return yet, there might be a triangle that is hit before this one!
+					#endif
+				}
+
+				++currentLeafIndex;
+			}
 		}
-	} while(currentNodeIndex != 0);
+		
+	} while(currentNodeIndex != 0 || nextIsLeafNode);
 
 #ifdef ANY_HIT
 	return false;
