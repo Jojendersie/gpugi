0d8549c89c5c8b55dae5a2558aee52d476de3d56
Johannes Jendersie <jojendersie@web.de>
2014-11-07 11:28:20 +0100

    fixed output file date (month was one to small) and swapped day and month for better sorting

--- gpugi/main.cpp
+++ gpugi/main.cpp
@@ -156,7 +156,7 @@ private:
 	{
 		time_t t = time(0);   // get time now
 		struct tm* now = localtime(&t);
-		std::string filename = m_screenShotName + " " + UIntToMinLengthString(now->tm_mday, 2) + "." + UIntToMinLengthString(now->tm_mon, 2) + " " +
+		std::string filename = m_screenShotName + " " + UIntToMinLengthString(now->tm_mon+1, 2) + "." + UIntToMinLengthString(now->tm_mday, 2) + " " +
 								UIntToMinLengthString(now->tm_hour, 2) + "h" + UIntToMinLengthString(now->tm_min, 2) + "m" + std::to_string(now->tm_sec) + "s.pfm";
 		gl::Texture2D& backbuffer = m_renderer->GetBackbuffer();
 		std::unique_ptr<ei::Vec4[]> imageData(new ei::Vec4[backbuffer.GetWidth() * backbuffer.GetHeight()]);
