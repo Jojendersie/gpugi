2eed97365b90ad30b80eadf3e19025ea4d03e54e
Andreas Reich <r_andreas2@web.de>
2014-11-10 23:48:04 +0100

    All material textures are now sampled only once per ray hit (unconditionally!) This should be faster since in "BRDF" all textures are at least sampled once - probability calling of BRDF() at least once should be quite high. However this changes raises the register pressure and introduces many data fetches in row which may or may not be maked by other computations. Currently there is a sligh performance *loss*. Still, I expect that it will improve for actual textures.

--- gpugi/shader/materials.glsl
+++ gpugi/shader/materials.glsl
@@ -6,9 +6,9 @@ vec3 SampleUnitHemisphere(vec2 randomSample, vec3 U, vec3 V, vec3 W)
     float r = sqrt(randomSample.y);
     float x = r * cos(phi);
     float y = r * sin(phi);
-    //float z = sqrt(saturate(1.0 - x*x - y*y));
-	float z = 1.0 - x*x -y*y;
-    z = z > 0.0 ? sqrt(z) : 0.0;
+    float z = sqrt(saturate(1.0 - x*x - y*y));
+	//float z = 1.0 - x*x -y*y;
+    //z = z > 0.0 ? sqrt(z) : 0.0;
 
     return x*U + y*V + z*W;
 }
@@ -30,13 +30,12 @@ vec3 SamplePhongLobe(vec2 randomSample, float exponent, vec3 U, vec3 V, vec3 W)
 
 // Sample a direction from the custom surface BRDF
 // weight x/p for the monte carlo integral. x weights the color channles/is the brdf
-vec3 SampleBRDF(vec3 incidentDirection, int material, inout uint seed, vec3 U, vec3 V, vec3 N, vec2 texcoord, out vec3 weight)
+vec3 SampleBRDF(vec3 incidentDirection, int material, vec4 reflectiveness, vec3 opacity, vec3 diffuse, inout uint seed, vec3 U, vec3 V, vec3 N, out vec3 weight)
 {
 	// THIS TWO LINES ARE ONLY HERE UNTIL THE OTHER STUFF RUNS
 	//weight = vec3(0.5);
 	//return SampleUnitHemisphere(Random2(seed), U, V, N);
 
-	vec4 reflectiveness = textureLod(sampler2D(Materials[material].reflectivenessTexHandle), texcoord, 0.0);
 
 	// Compute reflection probabilities with rescaled fresnel approximation from:
 	// Fresnel Term Approximations for Metals
@@ -64,11 +63,9 @@ vec3 SampleBRDF(vec3 incidentDirection, int material, inout uint seed, vec3 U, v
 		// TODO Resample in negative half space
 	} else {
 		// Refract/Absorb/Diffus
-		vec3 opacity = textureLod(sampler2D(Materials[material].opacityTexHandle), texcoord, 0.0).xyz;
 		float avgOpacity = (opacity.x + opacity.y + opacity.z) / 3.0;
 		if( avgOpacity > Random(seed) )
 		{
-			vec3 diffuse = textureLod(sampler2D(Materials[material].diffuseTexHandle), texcoord, 0.0).xyz;
 			// Normalize the probability
 			vec3 pdiffuse = (1.0 - preflect) * opacity;
 			float avgPDiffuse = (pdiffuse.x + pdiffuse.y + pdiffuse.z) / 3.0;
@@ -98,11 +95,9 @@ vec3 SampleBRDF(vec3 incidentDirection, int material, inout uint seed, vec3 U, v
 }
 
 // Sample the custom surface BRDF for two known direction
-vec3 BRDF(vec3 incidentDirection, vec3 excidentDirection, int material, vec3 N, vec2 texcoord)
+vec3 BRDF(vec3 incidentDirection, vec3 excidentDirection, int material, vec4 reflectiveness, vec3 opacity, vec3 diffuse, vec3 N)
 {
 	vec3 excidentLight = vec3(0.0);
-	vec4 reflectiveness = textureLod(sampler2D(Materials[material].reflectivenessTexHandle), texcoord, 0.0);
-
 	float cosTheta = -dot(N, incidentDirection);
 
 	// Compute reflection probabilities with rescaled fresnel approximation from:
@@ -117,11 +112,9 @@ vec3 BRDF(vec3 incidentDirection, vec3 excidentDirection, int material, vec3 N, 
 	excidentLight += phongNormalization * preflect * pow(max(0.0,dot(sampleDir, excidentDirection)), reflectiveness.w);
 
 	// Refract/Absorb/Diffus
-	vec3 opacity = textureLod(sampler2D(Materials[material].opacityTexHandle), texcoord, 0.0).xyz;
 	vec3 prefract = 1.0 - preflect;
 	vec3 pdiffuse = prefract * opacity;
 	prefract *= 1.0 - opacity;
-	vec3 diffuse = textureLod(sampler2D(Materials[material].diffuseTexHandle), texcoord, 0.0).xyz;
 	excidentLight += pdiffuse * diffuse / 3.141592654;// / pi?
 
 	// Refract
