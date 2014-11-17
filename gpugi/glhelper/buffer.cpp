19d62885a22aa93142d1c606363bbb250c21dd52
Andreas Reich <r_andreas2@web.de>
2014-11-17 12:58:40 +0100

    gl::Buffer supports now partial flush. gl::TextureBuffer supports now ranged buffer (Texture is only part of a actual buffer)

--- gpugi/glhelper/buffer.cpp
+++ gpugi/glhelper/buffer.cpp
@@ -116,11 +116,16 @@ namespace gl
 
 	void Buffer::Flush()
 	{
-		if(any(m_usageFlags & Usage::EXPLICIT_FLUSH))
+		Flush(m_mappedDataOffset, m_mappedDataSize);
+	}
+
+	void Buffer::Flush(std::uint32_t _offset, std::uint32_t _numBytes)
+	{
+		if (any(m_usageFlags & Usage::EXPLICIT_FLUSH))
 		{
 			// Flush only the part which was used.
 			GL_CALL(glFlushMappedNamedBufferRange, m_bufferObject,
-				m_mappedDataOffset, m_mappedDataSize);
+				_offset, _numBytes);
 		}
 	}
 
